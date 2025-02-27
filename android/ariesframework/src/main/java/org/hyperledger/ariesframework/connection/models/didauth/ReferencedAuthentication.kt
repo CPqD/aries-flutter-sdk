b3a6620dbae8eec456c956fbbbff6b821b03024e
Bruno Brasil <bruno-brasil.ferreira@serpro.gov.br>
2025-02-27 17:45:50 -0300

    Importar aries do nexus

--- android/ariesframework/src/main/java/org/hyperledger/ariesframework/connection/models/didauth/ReferencedAuthentication.kt
+++
@@ -1,9 +0,0 @@
-package org.hyperledger.ariesframework.connection.models.didauth
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-class ReferencedAuthentication(
-    val type: String,
-    val publicKey: String,
-) : Authentication()
